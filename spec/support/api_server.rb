1164a567b80f908d20fa15aa25ab7e4bfbd6fe56
Seth Vargo <sethvargo@gmail.com>
2014-01-30 11:51:22 -0500

    Add repository endpoints to the API server

--- spec/support/api_server.rb
+++ spec/support/api_server.rb
@@ -7,11 +7,33 @@ module Artifactory
   #
   class APIServer < Sinatra::Base
     require_relative 'api_server/artifact_endpoints'
+    require_relative 'api_server/repository_endpoints'
     require_relative 'api_server/status_endpoints'
     require_relative 'api_server/system_endpoints'
 
     register APIServer::ArtifactEndpoints
+    register APIServer::RepositoryEndpoints
     register APIServer::StatusEndpoints
     register APIServer::SystemEndpoints
+
+    private
+
+    #
+    # This server's URL, returned as a {Pathname} for easy joining.
+    #
+    # @example Construct a server url
+    #   server_url.join('libs-release-local', 'artifact.deb')
+    #
+    # @return [Pathname]
+    #
+    def server_url
+      @server_url ||= begin
+        scheme  = request.env['rack.url_scheme']
+        address = request.env['SERVER_NAME']
+        port    = request.env['SERVER_PORT']
+
+        Pathname.new("#{scheme}://#{address}:#{port}")
+      end
+    end
   end
 end
---
+++ spec/support/api_server/repository_endpoints.rb
@@ -0,0 +1,75 @@
+module Artifactory
+  module APIServer::RepositoryEndpoints
+    def self.registered(app)
+      app.get('/api/repositories') do
+        content_type 'application/vnd.org.jfrog.artifactory.repositories.RepositoryDetailsList+json'
+        JSON.fast_generate([
+          {
+            'key'         => 'libs-release-local',
+            'type'        => 'LOCAL',
+            'description' => 'Local repository for in-house libraries',
+            'url'         => server_url.join('libs-release-local'),
+          },
+          {
+            'key'         => 'libs-snapshots-local',
+            'type'        => 'LOCAL',
+            'description' => 'Local repository for in-house snapshots',
+            'url'         => server_url.join('libs-snapshots-local'),
+          },
+        ])
+      end
+
+      app.get('/api/repositories/libs-release-local') do
+        content_type 'application/vnd.org.jfrog.artifactory.repositories.LocalRepositoryConfiguration+json'
+        JSON.fast_generate({
+          'key'                          => 'libs-release-local',
+          'description'                  => 'Local repository for in-house libraries',
+          'notes'                        => '',
+          'includesPattern'              => '**/*',
+          'excludesPattern'              => '',
+          'repoLayoutRef'                => 'maven-2-default',
+          'enableNuGetSupport'           => false,
+          'enableGemsSupport'            => false,
+          'checksumPolicyType'           => 'client-checksums',
+          'handleReleases'               => true,
+          'handleSnapshots'              => false,
+          'maxUniqueSnapshots'           => 0,
+          'snapshotVersionBehavior'      => 'unique',
+          'suppressPomConsistencyChecks' => false,
+          'blackedOut'                   => false,
+          'propertySets'                 => ['artifactory'],
+          'archiveBrowsingEnabled'       => false,
+          'calculateYumMetadata'         => false,
+          'yumRootDepth'                 => 0,
+          'rclass'                       => 'local'
+        })
+      end
+
+      app.get('/api/repositories/libs-snapshots-local') do
+        content_type 'application/vnd.org.jfrog.artifactory.repositories.LocalRepositoryConfiguration+json'
+        JSON.fast_generate({
+          'key'                          => 'libs-snapshots-local',
+          'description'                  => 'Local repository for in-house snapshots',
+          'notes'                        => '',
+          'includesPattern'              => '**/*',
+          'excludesPattern'              => '',
+          'repoLayoutRef'                => 'maven-2-default',
+          'enableNuGetSupport'           => false,
+          'enableGemsSupport'            => false,
+          'checksumPolicyType'           => 'client-checksums',
+          'handleReleases'               => false,
+          'handleSnapshots'              => true,
+          'maxUniqueSnapshots'           => 0,
+          'snapshotVersionBehavior'      => 'unique',
+          'suppressPomConsistencyChecks' => false,
+          'blackedOut'                   => false,
+          'propertySets'                 => ['artifactory'],
+          'archiveBrowsingEnabled'       => false,
+          'calculateYumMetadata'         => false,
+          'yumRootDepth'                 => 0,
+          'rclass'                       => 'local'
+        })
+      end
+    end
+  end
+end
