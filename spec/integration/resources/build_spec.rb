7a528aab51a0b91abd9a9ae838af2485a715813c
Seth Vargo <sethvargo@gmail.com>
2014-01-30 16:12:41 -0500

    Add a pending spec for Build.all

---
+++ spec/integration/resources/build_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+module Artifactory
+  describe Resource::Build, :integration do
+    describe '.all' do
+      it 'returns an array of build objects' do
+        pending 'Need more information about what the Build API returns'
+      end
+    end
+  end
+end
