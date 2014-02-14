a119f09f5e94c8f536ef06b4799e9eaed136a7ee
Seth Vargo <sethvargo@gmail.com>
2014-02-14 14:19:08 -0500

    Don't use heredocs in the error text

--- lib/artifactory/errors.rb
+++ lib/artifactory/errors.rb
@@ -13,20 +13,17 @@ module Artifactory
         @code = hash['status'].to_i
         @http = hash['message'].to_s
 
-        super <<-EOH.strip
-The Artifactory server responded with an HTTP Error #{@code}: `#{@http}'"
-EOH
+        super "The Artifactory server responded with an HTTP Error "\
+              "#{@code}: `#{@http}'"
       end
     end
 
     # A general connection error with a more informative message
     class ConnectionError < ArtifactoryError
       def initialize(endpoint)
-        super <<-EOH.strip
-The Artifactory server at `#{endpoint}'
-is not currently accepting connections. Please ensure that the server is
-running an that your authentication information is correct.
-EOH
+        super "The Artifactory server at `#{endpoint}' is not currently " \
+              "accepting connections. Please ensure that the server is " \
+              "running an that your authentication information is correct."
       end
     end
   end
