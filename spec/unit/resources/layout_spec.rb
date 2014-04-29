f657675160daab434b352007e5e12d8e4f80e8ad
Yvonne Lam <yvonne@opscode.com>
2014-04-29 15:34:10 -0700

    Add unit tests for Layout.{find,all}.

--- spec/unit/resources/layout_spec.rb
+++ spec/unit/resources/layout_spec.rb
@@ -10,23 +10,33 @@ module Artifactory
     end
 
     describe '.all' do
-      let(:response) do
-        [
-          { 'name' => 'a' },
-          { 'name' => 'b' }
-        ]
+      let(:xml) do
+        REXML::Document.new('<config><repoLayouts><repoLayout><name>fake-layout</name></repoLayout></repoLayouts></config>')
+      end
+
+      before do
+        Resource::System.stub(:configuration).and_return(xml)
       end
 
       it 'returns the layouts' do
-        expect(described_class.all).to eq([ 'a', 'b'])
+        expect(described_class.all).to be_a(Array)
+        expect(described_class.all.first).to be_a(described_class)
+        expect(described_class.all.first.name).to eq('fake-layout')
       end
     end
 
     describe '.find' do
-      let(:response) { {} }
+      let(:xml) do
+        REXML::Document.new('<config><repoLayouts><repoLayout><name>found-layout</name></repoLayout></repoLayouts></config>')
+      end
 
-      it 'gets /api/repositories/#{name}' do
-        expect(client).to receive(response)
+      before do
+        Resource::System.stub(:configuration).and_return(xml)
+      end
+
+      it 'returns the found layout' do
+        expect(described_class.find('found-layout')).to be_a(described_class)
+        expect(described_class.find('found-layout').name).to eq('found-layout')
       end
     end
   end
